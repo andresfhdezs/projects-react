84c719cf00c60fc5b333ca2a95094f51172b1e76
Andres Hernandez <ahernandez@formax.cl>
2020-03-27 23:02:24 -0500

    se intalo firebase, react-redux-firebase, redux-firestore, and add reducers

--- src/components/projects/ProjectCreate.js
+++ src/components/projects/ProjectCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions'
 
 export class ProjectCreate extends Component {
 
@@ -15,7 +17,8 @@ export class ProjectCreate extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        console.log(this.state);
+        // console.log(this.state);
+        this.props.createProject(this.state);
     }
 
     render() {
@@ -42,4 +45,10 @@ export class ProjectCreate extends Component {
     }
 }
 
-export default ProjectCreate
+const mapDispatchToprops = (dispatch) => {
+    return {
+        createProject: (project) => dispatch(createProject(project))
+    }
+}
+
+export default connect(null, mapDispatchToprops)(ProjectCreate)
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { getFirebase } from 'react-redux-firebase';
+import { getFirestore } from 'redux-firestore';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })));
 
 ReactDOM.render(
   <React.StrictMode>
