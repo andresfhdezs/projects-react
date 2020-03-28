c312b71d1d60573c2ab20bbf1af49749e8ea2c7d
Andres Hernandez <ahernandez@formax.cl>
2020-03-28 00:38:59 -0500

    Fix index.js and get collection firestore with firestoreReducer on Dashboard.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,19 +1,19 @@
-import firebase, { firestore } from "firebase/app"
+import firebase/*, { firestore }*/ from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
 
 var config = {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDVVRcoxo8EAYO0wxi9JpKoWzqF-cOjP3s",
+    authDomain: "aplusexpress-676f4.firebaseapp.com",
+    databaseURL: "https://aplusexpress-676f4.firebaseio.com",
+    projectId: "aplusexpress-676f4",
+    storageBucket: "aplusexpress-676f4.appspot.com",
+    messagingSenderId: "338440469575",
+    appId: "1:338440469575:web:8a8d48924813c0f9528562",
+    measurementId: "G-HD0SBZ4P3Q"
 }
 
 firebase.initializeApp(config);
-firebase.firestore().settings({timestampsInSnapshots: true});
+firebase.firestore();
 
 export default firebase;
--- src/index.js
+++ src/index.js
@@ -3,22 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './store/reducers/rootReducer';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { getFirebase } from 'react-redux-firebase';
-import { getFirestore } from 'redux-firestore';
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
+import firebase from 'firebase/app'
 
-const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })));
+const store = createStore(
+  rootReducer,
+  compose(
+      applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+      reduxFirestore(firebase, fbConfig)
+  )
+);
+
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+          <App />
+      </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
