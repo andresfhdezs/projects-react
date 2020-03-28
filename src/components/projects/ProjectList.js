61517758c9d27baf2938fd868112676b88be2ed1
Andres Hernandez <ahernandez@formax.cl>
2020-03-27 21:23:35 -0500

    reducers listando contenido estatico

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,26 +2,17 @@ import React from 'react'
 import ProjectSummary from './ProjectSummary'
 
 
-const ProjectList = () => {
+const ProjectList = ({ projects }) => {
+    
     return (
         <div className="project-list section">
-            
-            <ProjectSummary />
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Titulo Proyecto</span>
-                    <p>Por: Andrés Hernández</p>
-                    <p className="text-grey">27 marzo, 2am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Titulo Proyecto</span>
-                    <p>Por: Andrés Hernández</p>
-                    <p className="text-grey">27 marzo, 2am</p>
-                </div>
-            </div>
+            {projects && projects.map(project => {
+                return (
+                    <ProjectSummary 
+                    key={project.id}
+                    project={project} />
+                )
+            })}
         </div>
     )
 }
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
+
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Titulo Proyecto</span>
+                <span className="card-title"> { project.title } </span>
                 <p>Por: Andrés Hernández</p>
                 <p className="text-grey">27 marzo, 2am</p>
             </div>
