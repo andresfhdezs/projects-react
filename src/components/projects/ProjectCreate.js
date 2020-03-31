16550cecc666f701daeee3f1005f2d670e48e208
Andres Hernandez <ahernandez@formax.cl>
2020-03-30 20:58:34 -0500

    Modificar contenido, para que se vea la fecha y formateada con el packeage moment y redirecciona cuando se crea uno nuevo

--- src/components/projects/ProjectCreate.js
+++ src/components/projects/ProjectCreate.js
@@ -18,8 +18,8 @@ export class ProjectCreate extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        // console.log(this.state);
         this.props.createProject(this.state);
+        this.props.history.push('/')
     }
 
     render() {
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 
 const ProjectSummary = ({project}) => {
 
@@ -6,8 +7,8 @@ const ProjectSummary = ({project}) => {
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title"> { project.title } | {project.id} </span>
-                <p>Por: Andrés Hernández</p>
-                <p className="text-grey">27 marzo, 2am</p>
+                <p>Por: { project.authorFirstName } { project.authorLastName }</p>
+                <p className="text-grey">{moment(project.createdAt.toDate()).calendar()}</p>
             </div>
         </div>
     )
